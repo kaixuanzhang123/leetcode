e122ea72af58caac50065c512c7f9f74d5315498
kaixuanzhang123 <zhangkaixuan@navinfo.com>
2022-09-05 10:52:21 +0800

    百度平衡二叉树

---
+++ src/main/java/com/kaikai/leetcode/baidu/平衡二叉树110.java
@@ -0,0 +1,53 @@
+package com.kaikai.leetcode.baidu;
+
+import com.kaikai.leetcode.二叉树的右视图199;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Package: com.kaikai.leetcode.baidu
+ * Description：
+ *
+ * @author: zhangkaixuan
+ * Date:  2022/9/5 9:14
+ * Modified By:
+ */
+public class 平衡二叉树110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int left = deeepLen(root.left);
+        int right = deeepLen(root.right);
+        return Math.abs(right - left) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int deeepLen(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = deeepLen(root.left);
+        int right = deeepLen(root.right);
+        return Math.max(right, left) + 1;
+    }
+
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
