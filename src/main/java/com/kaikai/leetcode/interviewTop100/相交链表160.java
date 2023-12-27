3097ce9cc4ac6a81b3dec2fa470dd8231abd4c75
zhangkaixuan.123 <zhangkaixuan.123@bytedance.com>
2023-12-27 13:36:21 +0800

    矩阵置零73 优化方法后面提交

---
+++ src/main/java/com/kaikai/leetcode/interviewTop100/相交链表160.java
@@ -0,0 +1,39 @@
+package com.kaikai.leetcode.interviewTop100;
+
+
+import com.kaikai.leetcode.meituan.回文链表;
+
+public class 相交链表160 {
+    //这是一道简单题
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+
