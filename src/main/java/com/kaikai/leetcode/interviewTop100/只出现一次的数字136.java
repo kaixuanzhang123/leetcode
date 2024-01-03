6394c891fc1fe527d6b5ee196f9789bb7ed48823
zhangkaixuan.123 <zhangkaixuan.123@bytedance.com>
2024-01-03 11:16:45 +0800

    只出现一次的数字136

---
+++ src/main/java/com/kaikai/leetcode/interviewTop100/只出现一次的数字136.java
@@ -0,0 +1,17 @@
+package com.kaikai.leetcode.interviewTop100;
+
+public class 只出现一次的数字136 {
+
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res = res ^ num;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+}
